96e551eb0b4bf2556b565d14375e33e29ba82ce3
JackVachon <36710461+JackVachon@users.noreply.github.com>
2018-02-21 19:56:02 -0500

    v2

---
+++ jsguess/jsguess.js
@@ -0,0 +1,17 @@
+
+function runStart(){
+	alert("Hello! One user will be prompted to enter a number, the second user will try to guess that number.");
+	var num1 = 0;
+	var num2 = 0;
+	
+	let numString = prompt("User 1, enter a positive number");
+	num1 = parseInt(numString);
+	
+	alert("User 1 has entered their number!');
+	
+	let numString2 = prompt("User 2, try and guess User 1's number");
+	num2 = parseInt(sumString2);
+	
+	
+	
+}
