efd9547e168cc5f09427491a8e8d1de0bd555c5e
JackVachon <36710461+JackVachon@users.noreply.github.com>
2018-03-21 14:52:36 -0400

    Works AGAIN

--- jqLight/jqLight.js
+++ jqLight/jqLight.js
@@ -6,21 +6,19 @@ $(function() {
 
 function myClickFunction() {
 	$("#button").on("click", function() {
-	switchOff = !switchOff;
-	if (switchOff) {
-		$("#body").on("click", function() {
-		$(this).css("background-color", "black");
-		$(this).css("color", "white");
-		$("#lightswitch").css("float", "right");
-		$("#button").text("Off");
-		});
-	}
-	else {
-		$("#body").css("background-color", "white");
-		$("#body").css("color", "black");
-		$("#lightswitch").css("float", "left");
-		$("#button").text("On");
-	}
+		switchOff = !switchOff;
+		if (switchOff) {
+			$("body").css("background-color", "black");
+			$("body").css("color", "white");
+			$("#lightswitch").css("float", "right");
+			$("#button").text("Off");
+		}
+		else {
+			$("#body").css("background-color", "white");
+			$("#body").css("color", "black");
+			$("#lightswitch").css("float", "left");
+			$("#button").text("On");
+		}
 	});
-};
+}
 
