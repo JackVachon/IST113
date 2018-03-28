4334add1e12e8f722b88bea0524b38c67d02c705
JackVachon <36710461+JackVachon@users.noreply.github.com>
2018-03-28 16:56:05 -0400

    HELP ME

---
+++ appTask3/appTask3.js
@@ -0,0 +1,98 @@
+var myKey = "listValues";
+var myItems = [];
+
+function runApp() {
+
+  if(localStorage.getItem(myKey) !== null) {
+    let myItemsString = localStorage.getItem(myKey);
+
+    myItems = JSON.parse(myItemsString);
+
+    $(myItems).each(function() {
+      createItem(this);
+    });
+  }
+  else {
+    createItem("List Goes Here");
+  }
+
+  //button
+  $("#buttonElement").on("click", function() {
+
+    //create button
+    let taskValue = $("#taskInput").val();
+
+    //call create item function
+    createItem(taskValue);
+
+    //delete items
+
+/*
+    $newElem = $("<li></li>").text(taskValue);
+    $("#taskList").append($newElem);
+    //delete button
+    let delButton = $('<button>',
+    {
+      text: "delete",
+      click: function() {
+        let itemText = $(this.parentNode).text();
+        let index = myItems.indexOf(itemText);
+        if(index != -1) {
+          myItems.splice(index, 1);
+          saveItems();
+        }
+        this.parentNode.remove();
+      }
+    });
+    */
+
+    //push item to array
+    myItems.push(taskValue);
+
+    //save array
+    saveItems();
+
+  });
+
+  $("#ClearButton").on("click", function() {
+    myItems = [];
+    saveItems();
+    $("ol").empty();
+  });
+}
+
+//create items function
+function createItem(itemValue) {
+  let $newElem = $("<li></li>").text(itemValue);
+  $("#RunningTasks").append($newElem);
+
+  //delete button
+  let delButton = $('<button>',
+  {
+    text: "delete",
+    click: function() {
+      removeItem($newElem);
+      $(this.parentNode).remove();
+    }
+  });
+ $newElem.append(delButton);
+}
+
+function removeItem(item) {
+  let index = item.index();
+
+  myItems.splice(index, 1);
+
+  saveItems();
+}
+
+//save items function
+function saveItems() {
+  let myItemsString = JSON.stringify(myItems);
+  localStorage.setItem(myKey, myItemsString);
+}
+
+//run the app
+$(function() {
+  runApp();
+});
